4a46d0b48a5621ad6d24ef7b3b5fac6476432382
vohrr <matthew.d.pingel@gmail.com>
2024-04-09 21:06:20 -0400

    -fixed snake growth and movement -implemented failure state when snake eats its own tail -added checks to make sure food doesn't spawn inside the snakes body

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ from Snake import Snake, SnakeNode
 #game window
 #game loop
 #event handler
-food_spawned = False
+food_spawned = True
 current_direction = None
 
 #we need a snake node class, it will contain the Rect object but also the current direction each node is 
@@ -22,12 +22,12 @@ def main(screen):
     global food_spawned
     global current_direction
     snake = Snake(screen)
-    food = new_food()
+    food = new_food(snake)
     while run:
         #refresh & redraw
         screen.fill('black')
         if not food_spawned:
-            food = new_food()
+            food = new_food(snake)
             food_spawned = True
         draw_snake(snake)
         pygame.draw.rect(screen, 'red', food)
@@ -49,41 +49,44 @@ def process_input(key,snake:Snake, food:Rect):
         check_collision(snake,food)
 
 #generate a coordinate that will be equal to our snake's size and have the same pixel centers        
-def new_food():    
+def new_food(snake:Snake):    
     x_cells = screen.get_width() / 40 - 1
     y_cells = screen.get_height() / 40 - 1
     x_coordinate = random.randint(0,int(x_cells)) * 40
     y_coordinate = random.randint(0,int(y_cells)) * 40
-    return pygame.Rect((x_coordinate,y_coordinate,40, 40))
+    food_candidate = pygame.Rect((x_coordinate,y_coordinate,40, 40))
+    if food_candidate.collidelist(snake.nodes) != -1:
+        return new_food(snake)
+    else: 
+        return food_candidate
+    
 
 def check_collision(snake:Snake, food:Rect):
     global food_spawned
-    global current_direction
     
     if snake.nodes[0].rect.colliderect(food):
         food_spawned = False
-        grow_snake(snake, current_direction)
+        grow_snake(snake)
     
-    #if snake[0].rect.collidelist(snake):
-        #pygame.quit()
+    if len(snake.nodes) > 1 and snake.nodes[0].rect.collidelist(snake.nodes[1:]) != -1:
+        pygame.event.clear
+        pygame.event.post(pygame.event.Event(pygame.QUIT))
         
 #current direction is used to append the next node of the snake to the tail
 #current direction is NOT the current direction of the head, its the current direction of the LAST INDEX
-def grow_snake(snake:Snake, direction:str):
+def grow_snake(snake:Snake):
     current_tail:SnakeNode = snake.nodes[-1]
     #TODO: implement so that the new tail is added properly based on current direction of old tail
     if current_tail.direction == 'left':
-        new_tail = SnakeNode(direction,pygame.Rect((current_tail.rect.right,current_tail.rect.top,40,40)))
+        new_tail = SnakeNode(current_tail.direction, pygame.Rect((current_tail.rect.right,current_tail.rect.top,40,40)))
     elif current_tail.direction == 'right':
-        new_tail = SnakeNode(direction,pygame.Rect((current_tail.rect.left,current_tail.rect.top,40,40)))
+        new_tail = SnakeNode(current_tail.direction, pygame.Rect((current_tail.rect.left-40,current_tail.rect.top,40,40)))
     elif current_tail.direction == 'up':
-        new_tail = SnakeNode(direction,pygame.Rect((current_tail.rect.left,current_tail.rect.top,40,40)))
+        new_tail = SnakeNode(current_tail.direction, pygame.Rect((current_tail.rect.left,current_tail.rect.bottom,40,40)))
     elif current_tail.direction == 'down':
-        new_tail = SnakeNode(direction,pygame.Rect((current_tail.rect.left,current_tail.rect.bottom,40,40)))
-    
+        new_tail = SnakeNode(current_tail.direction, pygame.Rect((current_tail.rect.left,current_tail.rect.top-40,40,40)))
     snake.nodes.append(new_tail)
-
-
+    
 def draw_snake(snake:Snake):
     for segment in snake.nodes:
         pygame.draw.rect(screen, 'green', segment.rect)
--- Move.py
+++ Move.py
@@ -10,7 +10,7 @@ MOVE_DOWN = [pygame.K_s, pygame.K_DOWN]
 
 MOVEMENT_VALUE = 40
 
-def move_snake_all(move_key, snake:Snake, screen):  
+def move_snake_all(move_key, snake:Snake, screen):
     head = snake.nodes[0].rect
     if move_key in MOVE_LEFT and head.left != 0:
         move_snake(snake, True, -1)
@@ -22,16 +22,39 @@ def move_snake_all(move_key, snake:Snake, screen):
         move_snake(snake, False, 1)
 
 def move_snake(snake:Snake, x:bool, direction:int):
-    for node in snake.nodes:
-        if x:
-            node.rect.move_ip(direction * MOVEMENT_VALUE,0)
-            if direction == -1:
-                node.direction = 'left'
-            else:
-                node.direction = 'right'
+    move_head(snake.nodes[0],x,direction)
+    for index in range(len(snake.nodes)-1,0,-1):
+        dir = get_direction(snake.nodes[index].direction)
+        move_node(snake.nodes[index],dir)
+        lead:SnakeNode = snake.nodes[index-1]
+        snake.nodes[index].direction = lead.direction
+
+def move_head(head:SnakeNode, x:bool, direction:int):
+    if x:
+        head.rect.move_ip(direction * MOVEMENT_VALUE,0)
+        if direction == -1:
+            head.direction = 'left'
         else:
-            node.rect.move_ip(0, direction * MOVEMENT_VALUE)
-            if direction == -1:
-                node.direction = 'up'
-            else:
-                node.direction = 'down'
+            head.direction = 'right'
+    else:
+        head.rect.move_ip(0, direction * MOVEMENT_VALUE)
+        if direction == -1:
+            head.direction = 'up'
+        else:
+            head.direction = 'down'
+
+
+def move_node(node:SnakeNode, x_y:tuple):
+    node.rect.move_ip(x_y[0] * MOVEMENT_VALUE, x_y[1] * MOVEMENT_VALUE)
+
+def get_direction(direction:str):
+    if direction == 'left':
+        return (-1,0)
+    if direction == 'right':
+        return (1,0)
+    if direction == 'up':
+        return (0,-1)
+    if direction == 'down':
+        return (0,1)
+    return 0
+
